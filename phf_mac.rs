77241338f4c484ae93de816deb3be3493d733de9
Steven Fackler <sfackler@gmail.com>
2014-01-21 18:42:31 -0800

    Removed unneeded parentheses

--- phf_mac.rs
+++ phf_mac.rs
@@ -122,7 +122,7 @@ fn expand_mphf_map(cx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> MacResult {
         }
     }
     let time = time::precise_time_s() - start;
-    if (os::getenv("PHF_STATS").is_some()) {
+    if os::getenv("PHF_STATS").is_some() {
         cx.parse_sess().span_diagnostic.span_note(sp, format!("PHF generation took {} seconds", time));
     }
 

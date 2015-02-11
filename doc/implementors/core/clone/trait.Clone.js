30986beb25fd53b1222678408833a0ac9fc73620
Steven Fackler <sfackler@gmail.com>
2015-02-11 19:41:23 +0000

    rebuild pages at b09a174a166c7744c5989bedc6ba68340f6f7fd1

--- doc/implementors/core/clone/trait.Clone.js
+++ doc/implementors/core/clone/trait.Clone.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors['phf_macros'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='phf_macros/util/enum.Key.html' title='phf_macros::util::Key'>Key</a>",];
+implementors['phf'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/core/hash/trait.Hash.js
+++ doc/implementors/core/hash/trait.Hash.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors['phf_macros'] = ["<a class='stability Unstable' title='Unstable: module was recently redesigned'></a>impl&lt;S&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a>&lt;S&gt; for <a class='enum' href='phf_macros/util/enum.Key.html' title='phf_macros::util::Key'>Key</a>",];
+implementors['phf'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

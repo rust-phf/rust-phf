fbbb53094e52efa19ff225d3d3ef2cbc00b4a7af
Steven Fackler <sfackler@gmail.com>
2015-02-17 22:18:10 -0800

    Add example to root module docs

--- phf/src/lib.rs
+++ phf/src/lib.rs
@@ -2,6 +2,37 @@
 //!
 //! Keys can be string literals, byte string literals, byte literals, char
 //! literals, or any of the fixed-size isizeegral types.
+//!
+//! # Examples
+//!
+//! ```rust
+//! #![feature(plugin)]
+//! #![plugin(phf_macros)]
+//!
+//! extern crate phf;
+//!
+//! #[derive(Clone)]
+//! pub enum Keyword {
+//!     Loop,
+//!     Continue,
+//!     Break,
+//!     Fn,
+//!     Extern,
+//! }
+//!
+//! static KEYWORDS: phf::Map<&'static str, Keyword> = phf_map! {
+//!     "loop" => Keyword::Loop,
+//!     "continue" => Keyword::Continue,
+//!     "break" => Keyword::Break,
+//!     "fn" => Keyword::Fn,
+//!     "extern" => Keyword::Extern,
+//! };
+//!
+//! pub fn parse_keyword(keyword: &str) -> Option<Keyword> {
+//!     KEYWORDS.get(keyword).map(|t| t.clone())
+//! }
+//! # fn main() {}
+//! ```
 #![doc(html_root_url="https://sfackler.github.io/rust-phf/doc")]
 #![feature(core, no_std)]
 #![warn(missing_docs)]

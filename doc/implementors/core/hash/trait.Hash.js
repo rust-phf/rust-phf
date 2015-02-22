a50ccf5bff45cba2a82ad25d60a5ab4a30c04e9a
Steven Fackler <sfackler@gmail.com>
2015-02-22 01:10:05 +0000

    rebuild pages at d4c189a2b060df33e7c97d6c1f0f430b68fc23b5

--- doc/implementors/core/hash/trait.Hash.js
+++ doc/implementors/core/hash/trait.Hash.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
-implementors['phf_macros'] = ["<a class='stability Unstable' title='Unstable: module was recently redesigned'></a>impl&lt;S&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a>&lt;S&gt; for <a class='enum' href='phf_macros/util/enum.Key.html' title='phf_macros::util::Key'>Key</a>",];
-implementors['phf'] = [];
+implementors['phf_macros'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='phf_macros/util/enum.Key.html' title='phf_macros::util::Key'>Key</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

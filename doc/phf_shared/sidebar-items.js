0f68cad3abf8d3586321f1bf9b0265440c6d3c93
Steven Fackler <sfackler@gmail.com>
2015-03-20 00:03:12 +0000

    rebuild pages at 092310344f2397b8bdc27894248f69a6c29ed5cc

--- doc/phf_shared/sidebar-items.js
+++ doc/phf_shared/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["displace",""]],"trait":[["PhfHash","A trait implemented by types which can be used in PHF data structures"]]});
+initSidebarItems({"trait":[["PhfHash","A trait implemented by types which can be used in PHF data structures"]],"fn":[["displace",""]]});

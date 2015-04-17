14970e80dc5c7928405fd8102d13893a26073c07
Steven Fackler <sfackler@gmail.com>
2015-04-17 07:02:03 +0000

    rebuild pages at 0d23afb0be5b1dadd21bca8c32e928da4b184f2c

--- doc/phf_macros/util/sidebar-items.js
+++ doc/phf_macros/util/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Entry",""]],"fn":[["create_map",""],["create_ordered_map",""],["create_ordered_set",""],["create_set",""]],"enum":[["Key",""]]});
+initSidebarItems({"fn":[["create_map",""],["create_ordered_map",""],["create_ordered_set",""],["create_set",""]],"struct":[["Entry",""]],"enum":[["Key",""]]});

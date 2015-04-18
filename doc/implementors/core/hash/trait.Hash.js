cd90d55ba36a48facad42df9edccfd3d1c2689c6
Steven Fackler <sfackler@gmail.com>
2015-04-18 06:56:41 +0000

    rebuild pages at 0d23afb0be5b1dadd21bca8c32e928da4b184f2c

--- doc/implementors/core/hash/trait.Hash.js
+++ doc/implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['phf_macros'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='phf_macros/util/enum.Key.html' title='phf_macros::util::Key'>Key</a>",];
+implementors['phf_macros'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='phf_macros/util/enum.Key.html' title='phf_macros::util::Key'>Key</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/phf_shared/trait.PhfHash.js
+++ doc/implementors/phf_shared/trait.PhfHash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['phf_shared'] = [];implementors['phf_macros'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='phf_shared/trait.PhfHash.html' title='phf_shared::PhfHash'>PhfHash</a> for <a class='enum' href='phf_macros/util/enum.Key.html' title='phf_macros::util::Key'>Key</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='phf_shared/trait.PhfHash.html' title='phf_shared::PhfHash'>PhfHash</a> for <a class='struct' href='phf_macros/util/struct.Entry.html' title='phf_macros::util::Entry'>Entry</a>",];
+implementors['phf_shared'] = [];implementors['phf_macros'] = ["impl <a class='trait' href='phf_shared/trait.PhfHash.html' title='phf_shared::PhfHash'>PhfHash</a> for <a class='enum' href='phf_macros/util/enum.Key.html' title='phf_macros::util::Key'>Key</a>","impl <a class='trait' href='phf_shared/trait.PhfHash.html' title='phf_shared::PhfHash'>PhfHash</a> for <a class='struct' href='phf_macros/util/struct.Entry.html' title='phf_macros::util::Entry'>Entry</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

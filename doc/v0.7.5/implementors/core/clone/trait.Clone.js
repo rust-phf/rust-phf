b2564056c452f5ffa66a4a61177b77cf368e2310
Steven Fackler <sfackler@gmail.com>
2015-09-22 16:00:23 -0700

    Release v0.7.5

---
+++ doc/v0.7.5/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['phf_macros'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='phf_macros/util/enum.Key.html' title='phf_macros::util::Key'>Key</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

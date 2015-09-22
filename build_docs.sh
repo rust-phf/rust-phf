0b91eae669259e555d797b68402d87c52e3b551c
Steven Fackler <sfackler@gmail.com>
2015-09-22 15:52:51 -0700

    Make doc generation more reasonable

--- build_docs.sh
+++ build_docs.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-mkdir doc
-
 for crate in $(echo phf_shared phf_macros phf phf_codegen phf_generator); do
-    rm -r $crate/target
-    mkdir -p $crate/target
-    (cd $crate/target && ln -s ../../doc)
-    (cd $crate && cargo doc --no-deps)
+    (cd $crate && rm -f Cargo.lock && CARGO_TARGET_DIR=../target cargo doc --no-deps)
 done

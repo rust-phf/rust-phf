c9e7a93f4d6f85a72651aba6187e4c956d8c1167
Steven Fackler <sfackler@gmail.com>
2015-11-08 18:07:59 -0800

    Release v0.7.7

--- phf_generator/src/lib.rs
+++ phf_generator/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url="http://sfackler.github.io/rust-phf/doc/v0.7.6")]
+#![doc(html_root_url="http://sfackler.github.io/rust-phf/doc/v0.7.7")]
 extern crate phf_shared;
 extern crate rand;
 

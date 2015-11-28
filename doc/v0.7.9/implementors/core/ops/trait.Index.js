5e1e33f20e04b0b485feec10554a7b455c217d92
Steven Fackler <sfackler@gmail.com>
2015-11-28 15:43:50 -0800

    Release v0.7.9

---
+++ doc/v0.7.9/implementors/core/ops/trait.Index.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['phf'] = ["impl&lt;'a, K, V, T: ?<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html' title='core::marker::Sized'>Sized</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;&amp;'a T&gt; for <a class='struct' href='phf/map/struct.Map.html' title='phf::map::Map'>Map</a>&lt;K, V&gt; <span class='where'>where T: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> + <a class='trait' href='phf/trait.PhfHash.html' title='phf::PhfHash'>PhfHash</a>, K: <a class='trait' href='https://doc.rust-lang.org/nightly/core/borrow/trait.Borrow.html' title='core::borrow::Borrow'>Borrow</a>&lt;T&gt;</span>","impl&lt;'a, K, V, T: ?<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html' title='core::marker::Sized'>Sized</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;&amp;'a T&gt; for <a class='struct' href='phf/ordered_map/struct.OrderedMap.html' title='phf::ordered_map::OrderedMap'>OrderedMap</a>&lt;K, V&gt; <span class='where'>where T: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> + <a class='trait' href='phf/trait.PhfHash.html' title='phf::PhfHash'>PhfHash</a>, K: <a class='trait' href='https://doc.rust-lang.org/nightly/core/borrow/trait.Borrow.html' title='core::borrow::Borrow'>Borrow</a>&lt;T&gt;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

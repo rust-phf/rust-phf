d35d05f46bc9026a4db06f78592efdf77c6ce61f
Steven Fackler <sfackler@gmail.com>
2016-01-01 15:43:48 -0800

    pass args to doc builds

--- build_docs.sh
+++ build_docs.sh
@@ -5,5 +5,5 @@ export CARGO_TARGET_DIR=target
 
 for toml in $(find . -maxdepth 2 -name "Cargo.toml"); do
     cargo update --manifest-path $toml || true
-    cargo doc --no-deps --manifest-path $toml
+    cargo doc --no-deps --manifest-path $toml "$@"
 done

cd71ef0ad1959b7d865f1060eedea79a18ffca62
Steven Fackler <sfackler@gmail.com>
2016-03-29 20:42:17 -0700

    Release v0.7.14

---
+++ doc/v0.7.14/phf_macros/util/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Key",""]],"fn":[["create_map",""],["create_ordered_map",""],["create_ordered_set",""],["create_set",""]],"struct":[["Entry",""]]});
---
+++ doc/v0.7.14/phf_shared/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["displace",""],["get_index","Return an index into `phf_generator::HashState::map`."],["hash","`key` is from `phf_generator::HashState::key`."],["split",""]],"trait":[["PhfHash","A trait implemented by types which can be used in PHF data structures."]]});
